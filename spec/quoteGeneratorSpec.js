c23ebe8f3ad4087f6f844a3fd97daebbf4026e93
Bassel Kanso <basselkanso82@gmail.com>
2020-01-03 22:23:56 +0200

    got rid of not needed test

--- spec/quoteGeneratorSpec.js
+++ spec/quoteGeneratorSpec.js
@@ -1,24 +1,5 @@
 const QuoteGenerator = require("../quoteGenerator.js");
 
-const quoteSentenceParts = {
-    firstPart:[
-        "Being happy",
-        "Living alone",
-        "Never doing",
-        "Having fun"
-    ],
-    secondPart:[
-        "is something we do",
-        "is what we need",
-        "is when we shouldn't do"
-    ],
-    ThirdPart:[
-        "even though we know we shouldn't",
-        "although we think we don't",
-        "just incase its not for us"
-    ]
-};
-
 describe("Quote generator tests", () => {
     let quote;
     beforeEach(() => {
@@ -33,12 +14,6 @@ describe("Quote generator tests", () => {
         expect(quote[0]).not.toBe("");
         expect(quote[1]).not.toBe("");
         expect(quote[2]).not.toBe("");
-    });
-
-    it("should return quote parts from the database", () => {
-        expect(quoteSentenceParts.firstPart.includes(quote[0])).toBe(true);
-        expect(quoteSentenceParts.secondPart.includes(quote[1])).toBe(true);
-        expect(quoteSentenceParts.ThirdPart.includes(quote[2])).toBe(true);
     });
 
     it("should return random quote parts", () => {
