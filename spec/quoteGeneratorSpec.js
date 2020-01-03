002a05e079c0ac3177fd176f86d562697601ee55
Bassel Kanso <basselkanso82@gmail.com>
2020-01-03 19:00:19 +0200

    created a beforeEach for the tests to avoid repetition

--- spec/quoteGeneratorSpec.js
+++ spec/quoteGeneratorSpec.js
@@ -1,8 +1,13 @@
 const QuoteGenerator = require("../quoteGenerator.js");
 
 describe("Quote generator tests", () => {
+    let quote;
+    
+    beforeEach(function() {
+        quote = QuoteGenerator.getQuote();
+    });
+
     it("should return a and array of 3 strings ", function() {
-        let quote = QuoteGenerator.getQuote();
         expect(quote.length).toBe(3);
     });
 });
