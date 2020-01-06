8ae4218a3dfc0f1ac7e13c5b23e80cc46c012349
Bassel Kanso <basselkanso82@gmail.com>
2020-01-06 21:03:14 +0200

    created function that creates element and appends to the parent element

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const generateQuoteButton = document.querySelector(".quote-generator__button");
 
+const createHTMLDivElementTo = (parentElement,contentOfElement) => {
+    const div = document.createElement('div');
+    div.innerHTML = contentOfElement;
+    parentElement.appendChild(div);
+}
+
 generateQuoteButton.addEventListener("click", (event)=>{
     event.preventDefault();
     const quotes = document.querySelector(".quote-generator__random-quote");
