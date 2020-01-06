0b08dd7704314a4eb569d07499ccfa62dc696350
Bassel Kanso <basselkanso82@gmail.com>
2020-01-06 19:17:07 +0200

    listened to click on button element

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const QuoteGenerator = require("../quoteGenerator.js");
 const QuoteSentenceParts = require("../quotePartsDatabase.js");
+const generateQuoteButton = document.getElementsByClassName("quote-generator__button");
+
+generateQuoteButton.addEventListener("click", ()=>{
+    const quote = document.getElementsByClassName("quote-generator__random-quote");
+    const quoteGenerator = new QuoteGenerator(QuoteSentenceParts);
+    
+});
